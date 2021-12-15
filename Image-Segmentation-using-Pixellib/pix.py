336c00d61be25f232d6994300ccca6023b8c1cc2
Puranjit Singh <puranjit13@gmail.com>
2021-12-15 16:47:33 -0600

    Add files via upload

---
+++ Image-Segmentation-using-Pixellib/pix.py
@@ -0,0 +1,6 @@
+import pix
+from pixellib.instance import instance_segmentation
+
+segment_image = instance_segmentation()
+segment_image.load_model("mask_rcnn_coco.h5")
+segment_image.segmentImage("newyork.jpg", show_bboxes = True, output_image_name = "output1.jpg")
