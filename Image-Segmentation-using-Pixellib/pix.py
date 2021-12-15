5891c3fa741620b47c62451a2e8ee8dc8519174b
Puranjit Singh <puranjit13@gmail.com>
2021-12-15 16:50:26 -0600

    Update pix.py

--- Image-Segmentation-using-Pixellib/pix.py
+++ Image-Segmentation-using-Pixellib/pix.py
@@ -7,5 +7,5 @@ segment_image = instance_segmentation()
 # Loading pre-trained model
 segment_image.load_model("mask_rcnn_coco.h5")
 
-# Insert any image file that you wnat to run mask-rcnn model 
+# Insert any image file that you wnat to run on pre-trained mask-rcnn model and output would be saved in output1.jpg 
 segment_image.segmentImage("newyork.jpg", show_bboxes = True, output_image_name = "output1.jpg")
