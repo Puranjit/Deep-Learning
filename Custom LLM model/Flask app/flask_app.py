b6506d4f78549bc0421194600f8c13da387785d9
Puranjit Singh Matharoo <puranjit13@gmail.com>
2023-10-14 13:32:00 -0400

    Add files via upload

---
+++ Custom LLM model/Flask app/flask_app.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct 13 20:31:15 2023
+
+@author: puran
+"""
+
+from flask import Flask, request, render_template, jsonify, Response
+from werkzeug.utils import secure_filename
+from my_pdf_processor import process_pdf_query
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({"error": "No file part in the request"}), 400
+
+        file = request.files['file']
+
+        if file.filename == '':
+            return jsonify({"error": "No file selected"}), 400
+
+        filename = secure_filename(file.filename)
+        file.save(filename)
+
+        question = request.form['question']
+        response = process_pdf_query(filename, question)
+
+        return render_template('upload.html', response=response)
+
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run()
